bdf15be70e8a036f8617c9fec5004461234a6f2f
devgianlu <altomanigianluca@gmail.com>
2019-10-06 22:21:13 +0200

    Silenced build a bit more + do not install openssl docs

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@
 cd c-ares
 echo "----- Build c-ares (`git describe --tags`) -----"
 autoreconf -i
-../androidbuildlib out_path=../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64"
+../androidbuildlib out_path=../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64" silent="$SILENT"
 cd ..
 
 
@@ -12,14 +12,14 @@ cd ..
 cd libexpat/expat
 echo "----- Build expat (`git describe --tags`) -----"
 ./buildconf.sh
-../../androidbuildlib out_path=../../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64"
+../../androidbuildlib out_path=../../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64" silent="$SILENT"
 cd ../..
 
 
 # Build zlib
 cd zlib
 echo "----- Build zlib (`git describe --tags`) -----"
-../androidbuildlib out_path=../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64" no_host="true"
+../androidbuildlib out_path=../libs minsdkversion=21 target_abis="armeabi-v7a x86 arm64-v8a x86_64" no_host="true" silent="$SILENT"
 cd ..
 
 
--- build_openssl.sh
+++ build_openssl.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 
+VERBOSE=1
+if [ "$SILENT" == "true" ]
+then
+	VERBOSE=0
+fi
+
 cd openssl
 INSTALL_DIR="$1"
 PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
@@ -8,25 +14,25 @@ echo "----- Build openssl (`git describe --tags`) -----"
 echo "++ Build openssl armeabi-v7a ++"
 ./Configure android-arm -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/armeabi-v7a"
 make clean
-make -j4
-make install
+make -j4 V=$VERBOSE
+make install_sw
 
 echo "++ Build openssl arm64-v8a ++"
 ./Configure android-arm64 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/arm64-v8a"
 make clean
-make -j4
-make install
+make -j4 V=$VERBOSE
+make install_sw
 
 echo "++ Build openssl x86 ++"
 ./Configure android-x86 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/x86"
 make clean
-make -j4
-make install
+make -j4 V=$VERBOSE
+make install_sw
 
 echo "++ Build openssl x86_64 ++"
 ./Configure android-x86_64 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/x86_64"
 make clean
-make -j4
-make install
+make -j4 V=$VERBOSE
+make install_sw
 
 cd ..
