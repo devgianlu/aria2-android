01d80b3606f8b3586a1aadb277f0430bafcee01f
devgianlu <altomanigianluca@gmail.com>
2019-10-06 22:44:35 +0200

    Even less verbose

--- build_openssl.sh
+++ build_openssl.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
-VERBOSE=1
+VERBOSE_FLAGS=""
 if [ "$SILENT" == "true" ]
 then
-	VERBOSE=0
+	VERBOSE_FLAGS="-s V=0"
+	echo "Using non-verbose mode"
 fi
 
 cd openssl
@@ -14,25 +15,25 @@ echo "----- Build openssl (`git describe --tags`) -----"
 echo "++ Build openssl armeabi-v7a ++"
 ./Configure android-arm -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/armeabi-v7a"
 make clean
-make -j4 V=$VERBOSE
+make -j4 $VERBOSE_FLAGS
 make install_sw
 
 echo "++ Build openssl arm64-v8a ++"
 ./Configure android-arm64 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/arm64-v8a"
 make clean
-make -j4 V=$VERBOSE
+make -j4 $VERBOSE_FLAGS
 make install_sw
 
 echo "++ Build openssl x86 ++"
 ./Configure android-x86 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/x86"
 make clean
-make -j4 V=$VERBOSE
+make -j4 $VERBOSE_FLAGS
 make install_sw
 
 echo "++ Build openssl x86_64 ++"
 ./Configure android-x86_64 -D__ANDROID_API__=21 --prefix="$INSTALL_DIR/x86_64"
 make clean
-make -j4 V=$VERBOSE
+make -j4 $VERBOSE_FLAGS
 make install_sw
 
 cd ..
